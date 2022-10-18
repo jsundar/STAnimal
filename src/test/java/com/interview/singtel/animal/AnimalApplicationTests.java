0fc80b7c777d57b7c0e9442126ad3df80d855815
jsundar <e0146927@u.nus.edu>
2022-10-18 11:44:50 +0800

    project setup commit

---
+++ src/test/java/com/interview/singtel/animal/AnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.singtel.animal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
